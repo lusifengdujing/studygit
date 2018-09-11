6730b518d96db335a0c45d5cae93a859483b9c69
LUSIFENG <lsf493362328@qq.com>
2018-09-11 14:23:54 +0800

    创建购物车实例

--- Weblearn/WebSite1/scripts/Vue.js
+++ Weblearn/WebSite1/scripts/Vue.js
@@ -1,76 +1,12 @@
 ﻿var app = new Vue({
-    el: '#app',
+    el: 'app',
     data: {
-        message: 'Hello Vue!'
-    }
-})
 
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: '页面加载于 ' + new Date().toLocaleString()
-    }
-})
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: true
-    }
-})
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            { text: '学习 JavaScript' },
-            { text: '学习 Vue' },
-            { text: '整个牛项目' }
-        ]
-    }
-})
-var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: 'Hello Vue.js!'
-    },
-    methods: {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
-    }
-})
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello Vue!'
-    }
-})
-
-//定义名为 todo-item 的新组件
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-})
-
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        groceryList: [
-            { id: 0, text: '蔬菜' },
-            { id: 1, text: '奶酪' },
-            { id: 2, text: '随便其它什么人吃的东西' }
-        ]
-    }
-})
-
-var app8 = new Vue({
-    el: '#example',
-    data: {
-        message:'Hello'
     },
     computed: {
-        //计算属性的getter
-        reversedMessage: function () {
-            return this.message.split('').reverse().join('')
-        }
-    }
+        
+    },
+    methods: {
+
+    },
 })
